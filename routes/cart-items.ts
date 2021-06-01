957472d8c947909062450d2ceb084a0508d7544b
adam <paddenadam@gmail.com>
2021-06-01 15:50:43 -0400

    finished all required problems

--- routes/cart-items.ts
+++ routes/cart-items.ts
@@ -5,7 +5,7 @@ import CartItem from '../model/CartItem';
 
 const route = express.Router();
 
-
+// 1. GET /cart-items
 route.get("/", async (req, res) => {
   try {
     const client = await getClient();
@@ -16,5 +16,74 @@ route.get("/", async (req, res) => {
     res.status(500).json({message: "Internal Server Error"});
   }
 });
+
+// 2. GET /cart-items/:id
+route.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const client = await getClient();
+    const item = await client.db().collection<CartItem>('cartItems').findOne({ _id : new ObjectId(id) });
+    if (item) {
+      res.json(item);
+    } else {
+      res.status(404).json({message: "Not Found"});
+    }
+  } catch (err) {
+    console.error("FAIL", err);
+    res.status(500).json({message: "Internal Server Error"});
+  }
+});
+
+// 3. PUT /cart-items
+route.post("/", async (req, res) => {
+  const item = req.body as CartItem;
+  try {
+    const client = await getClient();
+    const result = await client.db().collection<CartItem>('cartItems').insertOne(item);
+    item._id = result.insertedId;
+    res.status(201).json(item);
+  } catch (err) {
+    console.error("FAIL", err);
+    res.status(500).json({message: "Internal Server Error"});
+  }
+});
+
+// 4. PUT /cart-items/:id
+route.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const item = req.body as CartItem;
+  delete item._id;
+  try {
+    const client = await getClient();
+    const result = await client.db().collection<CartItem>('cartItems').replaceOne({ _id: new ObjectId(id) }, item);
+    if (result.modifiedCount === 0) {
+      res.status(404).json({message: "Not Found"});
+    } else {
+      item._id = new ObjectId(id);
+      res.json(item);
+    }
+  } catch (err) {
+    console.error("FAIL", err);
+    res.status(500).json({message: "Internal Server Error"});
+  }
+});
+
+// 5. DELETE /cart-items/:id
+route.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const client = await getClient();
+    const result = await client.db().collection<CartItem>('cartItems').deleteOne({ _id: new ObjectId(id) });
+    if (result.deletedCount === 0) {
+      res.status(404).json({message: "Not Found"});
+    } else {
+      res.status(204).end();
+    }
+  } catch (err) {
+    console.error("FAIL", err);
+    res.status(500).json({message: "Internal Server Error"});
+  }
+});
+
 
 export default route;
